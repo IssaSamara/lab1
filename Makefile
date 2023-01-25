d43931002ab1f53e9cf4a53f2d4e70a0d53c168d
U-LAPTOP-EDJGRBSR\Sam <isamara@csub.edu>
2023-01-25 06:47:17 -0800

    my first lab-1 upload to github.

---
+++ Makefile
@@ -0,0 +1,12 @@
+# Issa Samara
+# Make lab1.cpp
+#
+
+
+all: lab1
+
+lab1: lab1.cpp
+	g++ lab1.cpp -Wall -o lab1
+
+clean:
+	rm -f lab1
---
+++ lab1.cpp
@@ -0,0 +1,21 @@
+// Issa Samara
+// lab-1
+// 1/24/23
+//
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    
+    int a = rand() % 10 + 1; 
+    cout << a << endl;
+    cout << a << endl;
+    cout << "Hello World\n";
+
+    cout << "Hello World\n";
+
+    return 0;
+}
+
